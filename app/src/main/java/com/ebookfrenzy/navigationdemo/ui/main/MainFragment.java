1974652da78efbebc5647e62756138cfd2d411de
Jonathan Rodrigues <jonrodrigues24@gmail.com>
2021-07-18 22:33:28 -0600

    Crashes when extracting text from second fragment resource

---
+++ app/src/main/java/com/ebookfrenzy/navigationdemo/ui/main/MainFragment.java
@@ -0,0 +1,53 @@
+package com.ebookfrenzy.navigationdemo.ui.main;
+
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import androidx.lifecycle.ViewModelProvider;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.navigation.Navigation;
+import com.ebookfrenzy.navigationdemo.R;
+import android.widget.EditText;
+
+public class MainFragment extends Fragment {
+
+  private MainViewModel mViewModel;
+
+  public static MainFragment newInstance() {
+    return new MainFragment();
+  }
+
+  @Nullable
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.main_fragment, container, false);
+  }
+
+  @Override
+  public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+    super.onActivityCreated(savedInstanceState);
+    mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+
+    Button button = getView().findViewById(R.id.button);
+    button.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Navigation.findNavController(view).navigate(
+            R.id.mainToSecond);
+        EditText userText = getView().findViewById(R.id.userText);
+
+        MainFragmentDirections.MainToSecond action =
+            MainFragmentDirections.mainToSecond();
+        action.setMessage(userText.getText().toString());
+        Navigation.findNavController(view).navigate(action);
+      }
+    });
+  }
+}
+
